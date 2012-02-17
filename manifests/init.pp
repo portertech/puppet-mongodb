af0c60c862abc4d11127b2c6354510908ad17ced
Sean Porter <portertech@gmail.com>
2012-02-17 09:45:58 -0800

    indentation cleanup, update readme, prepare to roll a new version

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 # Notes:
 #  This class is Ubuntu specific.
-#  By Sean Porter, Gastown Labs Inc.
+#  By Sean Porter Consulting
 #
 # Actions:
 #  - Install MongoDB using a 10gen Ubuntu repository
@@ -14,7 +14,7 @@
 # Sample Usage:
 #  class { mongodb:
 #    replSet => "myReplicaSet",
-#    ulimit_nofile => 21000,
+#    ulimit_nofile => 20000,
 #  }
 #
 class mongodb(
@@ -24,44 +24,45 @@ class mongodb(
   $package = $mongodb::params::package
 ) inherits mongodb::params {
 
-	package { "python-software-properties":
-		ensure => installed,
-	}
-	
-	exec { "10gen-apt-repo":
-		path => "/bin:/usr/bin",
-		command => "echo '${mongodb::params::repository}' >> /etc/apt/sources.list",
-		unless => "cat /etc/apt/sources.list | grep 10gen",
-		require => Package["python-software-properties"],
-	}
-	
-	exec { "10gen-apt-key":
-		path => "/bin:/usr/bin",
-		command => "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10",
-		unless => "apt-key list | grep 10gen",
-		require => Exec["10gen-apt-repo"],
-	}
-	
-	exec { "update-apt":
-		path => "/bin:/usr/bin",
-		command => "apt-get update",
-		unless => "ls /usr/bin | grep mongo",
-		require => Exec["10gen-apt-key"],
-	}
+  package { "python-software-properties":
+    ensure => installed,
+  }
 
-	package { $mongodb::params::package:
-		ensure => installed,
-		require => Exec["update-apt"],
-	}
+  exec { "10gen-apt-repo":
+    path => "/bin:/usr/bin",
+    command => "echo '${mongodb::params::repository}' >> /etc/apt/sources.list",
+    unless => "cat /etc/apt/sources.list | grep 10gen",
+    require => Package["python-software-properties"],
+  }
 
-	service { "mongodb":
-		enable => true,
-		ensure => running,
-	}
+  exec { "10gen-apt-key":
+    path => "/bin:/usr/bin",
+    command => "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10",
+    unless => "apt-key list | grep 10gen",
+    require => Exec["10gen-apt-repo"],
+  }
+
+  exec { "update-apt":
+    path => "/bin:/usr/bin",
+    command => "apt-get update",
+    unless => "ls /usr/bin | grep mongo",
+    require => Exec["10gen-apt-key"],
+  }
+
+  package { $mongodb::params::package:
+    ensure => installed,
+    require => Exec["update-apt"],
+  }
+
+  service { "mongodb":
+    enable => true,
+    ensure => running,
+  }
 
   file { "/etc/init/mongodb.conf":
     content => template("mongodb/mongodb.conf.erb"),
     mode => "0644",
     notify => Service["mongodb"],
   }
+
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -12,15 +12,15 @@
 #  include mongodb::params
 #
 class mongodb::params {
-	$repository="deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen"
-	$package="mongodb-10gen"
+  $repository="deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen"
+  $package="mongodb-10gen"
 
-  # name of replica set (if any) to join
+  # Name of replica set (if any) to join
   $replSet = ""
 
-  # number of open files ulimit can be changed if mongodb.log reports
+  # Number of open files ulimit can be changed if mongodb.log reports
   # "too many open files" or "too many open connections" messages.
-  # mongodb has an upper limit of 20k.
+  # MongoDB has an upper hard limit of 20k.
   # http://www.mongodb.org/display/DOCS/Too+Many+Open+Files
   $ulimit_nofile = 1024
 }
